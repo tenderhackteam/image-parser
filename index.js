d2c622e4073cf0a332921a1c6f0b2d5560d4ce5e
Kirill Osintsev <osintsev.2000@inbox.ru>
2021-09-05 12:03:29 +0500

    Finalize

--- index.js
+++ index.js
@@ -13,17 +13,20 @@ app.get("/getImage", cors(corsOptions), async (request, response) => {
     const image_id = request.query.image_id;
 	const url = `https://zakupki.mos.ru/sku/view/${image_id}`;
 
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(url);
-	await page.waitForSelector('.image-gallery-slides > div > div > img');
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(url);
+		await page.waitForSelector('.image-gallery-slides > div > div > img');
 
-	await page.evaluateHandle(() => {
-        return document.querySelector('.image-gallery-slides > div > div > img').src;
-	}).then(data => response.send(data._remoteObject.value))
-	  .catch(err => response.sendStatus(500));
-
-	page.close();
+		await page.evaluateHandle(() => {
+			return document.querySelector('.image-gallery-slides > div > div > img').src;
+		}).then(data => response.send(data._remoteObject.value))
+		.catch(err => response.sendStatus(500));
+		page.close();
+	} catch {
+		response.sendStatus(500);
+	}
 });
 
 app.listen(3001);
