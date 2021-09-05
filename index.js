bbeffcbaeecace4d47de8aae432b0ee3746ff0d5
Kirill Osintsev <osintsev.2000@inbox.ru>
2021-09-05 07:56:33 +0500

    Parse image is redy

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require('cors');
+const rp = require('request-promise');
+const puppeteer = require('puppeteer');
+
+const app = express();
+app.use(cors())
+
+const corsOptions = {
+	origin: '*'
+};
+
+app.get("/getImage", cors(corsOptions), async (request, response) => {
+    const image_id = request.query.image_id;
+	const url = `https://zakupki.mos.ru/sku/view/${image_id}`;
+
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url);
+	await page.waitForSelector('.image-gallery-slides > div > div > img');
+
+	await page.evaluateHandle(() => {
+        return document.querySelector('.image-gallery-slides > div > div > img').src;
+	}).then(data => response.send(data._remoteObject.value))
+	  .catch(err => response.sendStatus(500));
+});
+
+app.listen(3001);
